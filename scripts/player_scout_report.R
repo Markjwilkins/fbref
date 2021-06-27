6afb0aad999ac294b3e1498cfbd8926b1b0b7852
Markjwilkins <72356860+Markjwilkins@users.noreply.github.com>
2021-06-27 16:47:41 +0100

    Update player_scout_report.R

--- scripts/player_scout_report.R
+++ scripts/player_scout_report.R
@@ -4,6 +4,7 @@ library(tidyverse)
 
 get_player_links <- function(){
   
+  ##select leagues - top 5 European
   leagues <- 
     c("ENG",
       "GER",
@@ -11,17 +12,20 @@ get_player_links <- function(){
       "ITA",
       "FRA")
   
+  ##retrieve league links
   league_links <- 
     fb_league_urls(country = leagues,
                    season_end_year = 2021,
                    gender = "M",
                    tier = "1st")
   
+  ##retrieve team links
   team_links <- 
     league_links %>% 
     map(~fb_teams_urls(league_url = .)) %>% 
     unlist
   
+  ##get player links
   player_links <- 
     team_links %>% 
     map(~fb_player_urls(team_url = .)) %>% 
@@ -30,10 +34,20 @@ get_player_links <- function(){
 
 player_links <- get_player_links()
 
+##get player scout report for all players in top 5 
 df <- 
   player_links %>% 
   map(possibly(~fb_player_scouting_report(player_url = .,
                                     pos_versus = "primary"),
                   otherwise = "NA"))
 
+##create df
 data <- do.call(rbind, df) 
+
+##remove NA players
+data <- 
+  data %>% 
+  filter(Player != "NA")
+
+##save csv output
+write_csv(data, "all_player_scout_report_2021.csv")
